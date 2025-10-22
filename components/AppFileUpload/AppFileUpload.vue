5fc0ff7f5013522fdc1c018fba852aee3bb52574
Francisco Diosdado <drz.y2k@gmail.com>
2025-10-21 22:15:09 -0600

    Validate File Upload Filetype With Javascript

--- components/AppFileUpload/AppFileUpload.vue
+++ components/AppFileUpload/AppFileUpload.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 const files = ref<File[]>([]);
-function handleFileSelect(event:Event) {
+
+//agregamos una lista de mimetypes permitidos
+const allowedFileTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+
+function handleFileSelect(event: Event) {
 	//accedemos al elemento que disparó el evento, indicamos a ts que es un htmlInputElement
 	//con esto podemos acceder a la propiedad files
-	const input=event.target as HTMLInputElement;
+	const input = event.target as HTMLInputElement;
 	//input files es un objeto que contiene los archivos seleccionados
 	//lo convertimos a un array con Array.from
 	//si por algo es undefined, le pasamos un array vacío para evitar errores
@@ -17,12 +21,20 @@ function handleFileSelect(event:Event) {
 <template>
 	<div>
 		<!-- follow along here -->
-		 <!-- input oculto con atributo multiple para seleccionar varios archivos -->
+		<!-- input oculto con atributo multiple para seleccionar varios archivos -->
 		<label class="px-4 py-2 bg-blue-500 text-white rounded cursor-pointer">
 			<input type="file" multiple @change="handleFileSelect" hidden />
 			Upload
 		</label>
-		<p v-for="file in files" :key="file.name">{{ file.name }}</p>
+		<p v-for="file in files" :key="file.name">
+			{{ file.name
+			}}
+			<!-- si el archivo que vamos a mostrar no está en los mimetypes permitidos
+			mostramos un mensaje de error -->
+			<span v-if="!allowedFileTypes.includes(file.type)" class="text-red-500">
+				File type not allowed
+			</span>
+		</p>
 	</div>
 </template>
 
