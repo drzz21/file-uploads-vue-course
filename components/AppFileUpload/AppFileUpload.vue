e2403dd98727dcbf9b129bdc560ef108dfd56f48
Francisco Diosdado <drz.y2k@gmail.com>
2025-10-21 22:07:07 -0600

    Store Selected Files in a Vue.js Variable Ref

--- components/AppFileUpload/AppFileUpload.vue
+++ components/AppFileUpload/AppFileUpload.vue
@@ -1,16 +1,28 @@
-<script setup>
-function handleFileSelect(event) {
-	console.log(event);
+<script setup lang="ts">
+const files = ref<File[]>([]);
+function handleFileSelect(event:Event) {
+	//accedemos al elemento que disparó el evento, indicamos a ts que es un htmlInputElement
+	//con esto podemos acceder a la propiedad files
+	const input=event.target as HTMLInputElement;
+	//input files es un objeto que contiene los archivos seleccionados
+	//lo convertimos a un array con Array.from
+	//si por algo es undefined, le pasamos un array vacío para evitar errores
+	const fileAsArray = Array.from(input?.files || []); // FileList
+	//concatenamos los archivos seleccionados a nuestra referencia reactiva
+	files.value = files.value.concat(fileAsArray);
+	// console.log(event);
 }
 </script>
 
 <template>
 	<div>
 		<!-- follow along here -->
+		 <!-- input oculto con atributo multiple para seleccionar varios archivos -->
 		<label class="px-4 py-2 bg-blue-500 text-white rounded cursor-pointer">
-			<input type="file" @change="handleFileSelect" hidden />
-      Upload
+			<input type="file" multiple @change="handleFileSelect" hidden />
+			Upload
 		</label>
+		<p v-for="file in files" :key="file.name">{{ file.name }}</p>
 	</div>
 </template>
 
